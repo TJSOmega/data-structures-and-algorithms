b56beb68b194c4c6133873a2117f5589e86cfd0a
Terrence Jamal Simms <69491583+TJSOmega@users.noreply.github.com>
2021-05-26 23:26:42 -0700

    Started on Hash Tables

---
+++ javascript/Data-Structures/hashTable/hashmap.js
@@ -0,0 +1,24 @@
+// CLASS EXAMPLE
+
+class HashMap {
+  constructor(size) {
+    this.size = size;
+    this.map = new Array.size(size).fill()
+  }
+
+  hash(key) {
+    return 4;
+  }
+
+  set(key, value) {
+
+  }
+  // Returns a value in the Hash Map
+  get(key) {
+
+  }
+  // Returns a boolean regarding if the object is in the Hash Map
+  has(key) {
+
+  }
+}
